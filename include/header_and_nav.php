857a1520671a1af9ce56aa5c9c82c377c21977ea
snooji27 <121448423+snooji27@users.noreply.github.com>
2025-05-02 04:39:05 +0300

    added my footer and header
    added a new folder "include", it has footer and header files that i used for "gamestore" and "games.php"

---
+++ include/header_and_nav.php
@@ -0,0 +1,23 @@
+<header>
+  <div class="logo-container">
+    <a href="homepage.html"><img src="Media/icon2.png" alt="Logo" class="header-logo"></a>
+  </div>
+  <nav>
+    <ul>
+      <li>
+      <a href="cart.php" class="cart-icon">
+          <i class="fas fa-shopping-cart"></i>
+          <span id="cart-count">
+          </span>
+      </a>
+      </li>
+      <li><a href="homepage.html">Home</a></li>
+      <li><a href="gamestore.php">Games</a></li>
+      <li><a href="homepage.html#about">About Us</a></li>
+      <li><a href="support.html">Contact</a></li>
+      <li><a href="Login_Signup/selection.html" class="login-btn">Login</a></li>
+      <li><a href="Login_Signup/selection.html" class="signup-btn">Sign Up</a></li>
+      
+    </ul>
+  </nav>
+</header>
